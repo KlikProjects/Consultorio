532c54386f3ce4f05d50dd6449b77654ea3695cb
blaucomfuig <scorcuera6@gmail.com>
2021-05-13 13:07:12 +0200

    se consiguió realizar la conexión de datos

---
+++ Sergi/src/Database.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class Database
+{
+
+    public $mysql;
+
+    public function __construct()
+    {
+        try {
+            $this->mysql = $this->getConnection();
+         
+           
+        } catch (PDOException $ex) {
+            echo "Ooooops, sois muy malos para el código !" . $ex->getMessage();
+        }
+    }
+
+    private function getConnection()
+    {
+
+        $host = "localhost";
+        $user = "root";
+        $pass = "";
+        $database = "elconsultorio";
+        $charset = "utf-8";
+        $options = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+        $pdo = new pdo("mysql:host={$host};dbname={$database};charset{$charset}", $user, $pass, $options);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    }
+}
+
+
+$test = new Database();
+$query = $test -> mysql -> query('SELECT * FROM citascoders');
+print_r($query ->fetchAll());
+
+
