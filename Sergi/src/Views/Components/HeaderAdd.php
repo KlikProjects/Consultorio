1e071bb4fac0d2e3854db70339de85244a1c816b
blaucomfuig <scorcuera6@gmail.com>
2021-05-20 17:44:33 +0200

    habilitar visión de CTO

--- Sergi/src/Views/Components/HeaderAdd.php
+++ Sergi/src/Views/Components/HeaderAdd.php
@@ -1,4 +1,10 @@
 <header class="headerUsuarioEdit">
     <div class="containerUsuarioEdit"></div>
     <h1 class="txtHeader">HANDY APP</h1>
+    <a href="?action=indexCTO">
+        <button>
+            CTO
+        </button>
+    </a>
+   
 </header>
